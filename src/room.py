29d5e0959be4e1187266c2dbd210a3afa925e97f
hectorsvill <hectorstevenvillasano@gmail.com>
2019-10-02 23:15:05 -0700

    room

--- src/room.py
+++ src/room.py
@@ -2,7 +2,7 @@
 # description attributes.
 
 class Room:
-    def __init__(self,name, description, n_to, s_to, e_to, w_to):
+    def __init__(self,name, description, n_to = [], s_to = [], e_to = [], w_to = []):
         self.name = name
         self.description = description
         self.n_to = n_to
@@ -10,3 +10,4 @@ class Room:
         self.e_to = e_to
         self.w_to = w_to
 
+        
